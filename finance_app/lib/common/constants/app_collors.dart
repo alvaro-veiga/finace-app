a4bd62170bdb55c6702f675059a5f50317eea251
Alvaro Veiga <alvarojoao17@gmail.com>
2024-10-03 22:47:46 -0300

    feat: create onboarding page

--- finance_app/lib/common/constants/app_collors.dart
+++ finance_app/lib/common/constants/app_collors.dart
@@ -6,4 +6,6 @@ class AppCollors {
   static const Color greenlightOne = Color(0xff63b5af);
   static const Color greenlightTwo = Color(0xFF438883);
   static const Color white = Color(0xffffffff);
+  static const Color iceWhite = Color(0xFFEEF8F7);
+  static const Color grey = Color(0xff444444);
 }
---
+++ finance_app/lib/features/onboarding/onboarding_page.dart
@@ -0,0 +1,49 @@
+import 'package:finance_app/common/constants/app_collors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Align(
+        child: Column(
+        children: [
+          const SizedBox(height: 60.0,),
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: AppCollors.iceWhite,
+              child: Image.asset("assets/images/man.png"),
+              )
+          ),
+          const Text('Spend smarter', style: TextStyle(
+            fontFamily: 'inter',
+            fontSize: 36.0,
+            fontWeight: FontWeight.w700,
+            color: AppCollors.greenlightTwo
+          ),),
+          const Text('Save more', style: TextStyle(
+            fontFamily: 'inter',
+            fontSize: 36.0,
+            fontWeight: FontWeight.w700,
+            color: AppCollors.greenlightTwo
+          ),),
+          OutlinedButton(
+            onPressed: () {}, 
+            child: const Text('Get Started')),
+          const Text('Already have account? Log In', style: TextStyle(
+            fontFamily: 'inter',
+            fontSize: 14.0,
+            fontWeight: FontWeight.w500,
+            color: AppCollors.grey
+          ),)
+        ],
+      ),
+      ),
+      
+    );
+  }
+}
