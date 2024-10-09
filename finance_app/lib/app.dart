3a483f79d60806aba715179bcdf9073a4c3be8fd
Alvaro Veiga <alvarojoao17@gmail.com>
2024-10-08 21:15:27 -0300

    feat: create default theme

--- finance_app/lib/app.dart
+++ finance_app/lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:finance_app/common/themes/default_theme.dart';
 import 'package:finance_app/features/onboarding/onboarding_page.dart';
 import 'package:finance_app/features/sign_up/sign_up_page.dart';
 import 'package:finance_app/features/splash/splash_page.dart';
@@ -8,8 +9,9 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SignUpPage(),
+    return MaterialApp(
+      theme: defaultTheme,
+      home: const SignUpPage(),
     );
 
   }
--- finance_app/lib/features/sign_up/sign_up_page.dart
+++ finance_app/lib/features/sign_up/sign_up_page.dart
@@ -10,52 +10,74 @@ class SignUpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
+        padding: const EdgeInsets.all(16.0),
         children: [
-           const Text('Start Saving',
+          const Text(
+            'Start Saving',
             textAlign: TextAlign.center,
-            style: TextStyle(
-            fontFamily: 'inter',
-            fontSize: 36.0,
-            fontWeight: FontWeight.w700,
-            color: AppCollors.greenlightTwo
-          ),),
-          const Text('Your Money!',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-            fontFamily: 'inter',
-            fontSize: 36.0,
-            fontWeight: FontWeight.w700,
-            color: AppCollors.greenlightTwo
-          ),
-          ),
-          Expanded(
-            child: Image.asset('assets/images/sing_up.png')
-          ),
-          TextFormField(),
-          const TextField(),
-          PrimaryButton(text: 'Sign Up', onPressed: () {},),
-          CustomTextButton(
-          onPressed: () {},
-          children: [ 
-          Text(
-            'Already have account? ',
             style: TextStyle(
               fontFamily: 'inter',
-              fontSize: 14.0,
-              fontWeight: FontWeight.w500,
-              color: AppCollors.grey
-            )
+              fontSize: 36.0,
+              fontWeight: FontWeight.w700,
+              color: AppCollors.greenlightTwo,
+            ),
           ),
-        Text(
-          'Log in ',
-          style: TextStyle(
-            fontFamily: 'inter',
-            fontSize: 14.0,
-            fontWeight: FontWeight.w500,
-            color: AppCollors.greenlightTwo
-          )
-        )],
-        ),
+          const Text(
+            'Your Money!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'inter',
+              fontSize: 36.0,
+              fontWeight: FontWeight.w700,
+              color: AppCollors.greenlightTwo,
+            ),
+          ),
+          const SizedBox(height: 20.0),
+          Image.asset('assets/images/sing_up.png'),
+          const SizedBox(height: 20.0),
+          Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Your Name",
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+              ],
+            ),
+          ),
+          PrimaryButton(
+            text: 'Sign Up',
+            onPressed: () {},
+          ),
+          const SizedBox(height: 10.0),
+          CustomTextButton(
+            onPressed: () {},
+            children: [
+              Text(
+                'Already have account? ',
+                style: TextStyle(
+                  fontFamily: 'inter',
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w500,
+                  color: AppCollors.grey,
+                ),
+              ),
+              Text(
+                'Log in ',
+                style: TextStyle(
+                  fontFamily: 'inter',
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w500,
+                  color: AppCollors.greenlightTwo,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
